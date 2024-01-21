3a454788a21e57761795b6c68da4113f0c5c3c8a
nicolaics <nicolai.christian.s@gmail.com>
2024-01-21 16:32:03 +0700

    text color change according to themes

--- app/src/main/java/xml_csv/xmlcsv/MedicineRadioButtonAdaptor.kt
+++ app/src/main/java/xml_csv/xmlcsv/MedicineRadioButtonAdaptor.kt
@@ -1,11 +1,13 @@
 package xml_csv.xmlcsv
 
 import android.content.Context
+import android.content.res.Configuration
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
 import android.widget.CheckBox
+import androidx.core.content.ContextCompat
 
 class MedicineRadioButtonAdaptor(val context: Context, val searchedResult : ArrayList<Medicine>,
                                  var selectMedicineList : ArrayList<SearchedMedicine>, val index : Int ) : BaseAdapter() {
@@ -30,6 +32,13 @@ class MedicineRadioButtonAdaptor(val context: Context, val searchedResult : Arra
         medicineCheckBox.text = searchedResult[p0].medicineName
 
         val chosenMedicine = medicineCheckBox.text.toString()
+
+        when(context.resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK) {
+            Configuration.UI_MODE_NIGHT_YES -> {
+                medicineCheckBox.setTextColor(ContextCompat.getColor(context, R.color.white))
+            }
+            else -> medicineCheckBox.setTextColor(ContextCompat.getColor(context, R.color.black))
+        }
 
         medicineCheckBox.setOnCheckedChangeListener { button, isChecked ->
             if(isChecked){
